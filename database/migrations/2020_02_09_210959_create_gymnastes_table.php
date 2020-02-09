894a77b1334024932258660a680a802bb7850ebc
gruel <gruel@emse.fr>
2020-02-09 22:14:30 +0100

    models

---
+++ database/migrations/2020_02_09_210959_create_gymnastes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGymnastesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gymnastes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('nom');
+            $table->Integer('annee');
+
+            $table->timestamps();
+
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gymnastes');
+    }
+}
